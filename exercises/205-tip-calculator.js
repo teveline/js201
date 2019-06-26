7a18e373eec93c2078ca16edc56291489e47e5d1
Terrence Eveline <teveline@EvatosthenesMacBP.attlocal.net>
2019-06-26 00:30:15 -0500

    Completed tip-calculator. My mistake was mixing up the assign '=' operator with the equal to '==' operator.

--- exercises/205-tip-calculator.js
+++ exercises/205-tip-calculator.js
@@ -9,7 +9,15 @@
 // Examples:
 // tipAmount(100, 'good') --> 20
 // tipAmount(40, 'fair') --> 6
-
+function tipAmount (billAmount, service) {
+    if (service == 'good') {
+        return billAmount * 0.20;
+    } else if (service == 'fair') {
+        return billAmount * .15;
+    } else if (service == 'poor') {
+        return billAmount * 0.10;
+        }
+    }
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -20,9 +28,18 @@
 // Examples:
 // totalAmount(100, 'good') --> 120
 // totalAmount(40, 'fair') --> 46
-
-
-
+function totalAmount (billAmount, service) {
+    if (service == 'good') {
+        return billAmount + (billAmount * .2)
+    } 
+    else if (service == 'fair') {
+        return billAmount + (billAmount * .15)
+    } 
+    else if (service == 'poor') {
+        return billAmount + (billAmount * .1)
+    }
+}
+// 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "splitAmount" that takes a bill amount, the level of service,
 // and the number of people to split the bill between. It should return the final
@@ -31,3 +48,12 @@
 // Examples:
 // splitAmount(100, 'good', 5) --> 24
 // splitAmount(40, 'fair', 2) --> 23
+function splitAmount (billAmount, service, numOfPeople) {
+    if (service == 'good') {
+        return (billAmount + (billAmount * .2)) / numOfPeople
+    } else if (service == 'fair') {
+        return (billAmount + (billAmount * .15)) / numOfPeople
+    } else if (service == 'poor') {
+        return (billAmount + (billAmount * .1)) / numOfPeople
+    }
+}
