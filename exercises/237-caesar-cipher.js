fdc791a51b6e536362d4017cc6ce3ad353eead98
Terrence Eveline <tijayeve@gmail.com>
2019-07-17 21:58:58 -0500

    Completed leetspeak, began Caesar cipher

--- exercises/237-caesar-cipher.js
+++ exercises/237-caesar-cipher.js
@@ -19,3 +19,21 @@
 // 'attack at dawn!'
 // > decipher('ehz czlod otgpcrpo ty l hzzo', 11)
 // 'two roads diverged in a wood'
+
+function cipher (string, shift) {
+    let letters = "abcdefghijklmnopqrstuvwxyz"
+    let smallString = string.toLowerCase()
+    let codeString = ''
+
+    for (let idx = 0; idx < string.length; idx++) {
+        let charPos = smallString.charAt(idx);
+        let newChar =                               //new character must be related to its position in characters
+
+        if (letters.includes(charPos)) {
+            console.log(newChar)
+        } else codeString += charPos
+    }
+
+    return codeString
+
+}
